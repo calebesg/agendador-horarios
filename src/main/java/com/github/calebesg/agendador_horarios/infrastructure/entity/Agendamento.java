de71e3c51ba330d0deda79689677a364bd3c3b8b
calebesg <calebeguimaraes10@gmail.com>
2026-02-08 08:48:16 -0300

    initialize scheduling project

---
+++ src/main/java/com/github/calebesg/agendador_horarios/infrastructure/entity/Agendamento.java
@@ -0,0 +1,30 @@
+package com.github.calebesg.agendador_horarios.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="agendamento")
+public class Agendamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String produto;
+
+    private LocalDateTime dataHoraAgendamento;
+
+    private String cliente;
+
+    private LocalDateTime dataInsercao = LocalDateTime.now();
+}
